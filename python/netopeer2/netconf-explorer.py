6f9c3956b00cc455f74596e3abbaa7470c5960eb
tud7 <tduongcs@gmail.com>
2022-04-12 23:17:35 -0700

    added demo with turing machine test yang module

--- python/netopeer2/netconf-explorer.py
+++ python/netopeer2/netconf-explorer.py
@@ -6,20 +6,6 @@ import xml.dom.minidom
 
 from ncclient import manager
 
-'''mgr = manager.connect(host='sandbox-iosxe-latest-1.cisco.com',
-                    port=830,
-                    username='developer',
-                    password='C1sco12345',
-                    hostkey_verify=False,
-                    device_params={'name':'csr'})
-'''                 
-'''
-mgr = manager.connect(host='localhost',
-                    port=1831,
-                    username='root',
-                    password='root',
-                    hostkey_verify=False)
-'''
 mgr = manager.connect(host='localhost',
                     port=830,
                     username='netconf',
@@ -34,87 +20,46 @@ def cli():
 @cli.command()
 def get_capability():
     """Get NETCONF Capabilities"""
-    
     for capability in mgr.server_capabilities:
         print(capability)
 
+
 @cli.command()
-@click.option("-n", "--schema-name", default="ietf-netconf-monitoring", help="Schema Name")
+@click.argument("schema-name")
 def get_schema(schema_name:str):
-    """Get schema"""
-    print( mgr.get_schema(schema_name) )
+    """Get NETCONF Schema"""
+    if schema_name:
+      print( mgr.get_schema(schema_name) )
     
     
 @cli.command()
-def get_config():
-    """Get Config"""
-    netconf_reply = mgr.get_config('running')
+@click.option("-f", "--filter-file", help="Full path to the filter XML file", default=None)
+def get_config(filter_file):
+    """Get NETCONF Config"""
+    
+    filter_xml = None
+
+    if filter_file is not None:
+      with open(filter_file, "r") as ff:
+         #read whole file to a string
+         filter_xml = ff.read()
+ 
+    netconf_reply = mgr.get_config('running', filter=filter_xml)
     config_dom    = xml.dom.minidom.parseString(netconf_reply.xml)
     print( config_dom.toprettyxml(indent='   ') )
 
 
 @cli.command()
-@click.option("-c", "--config-file", help="Full path to config file")
+@click.option("-f", "--config-file", help="Full path to config file")
 def edit_config(config_file:str):
-    """Edit Config"""
-    
-    """CONFIGURATION  = 
-        <config xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-            <native xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-                <hostname>tmd12345</hostname>
-            </native>
-        </config>
-    """
-    '''
-    CONFIGURATION  = """
-    <config xmlns="urn:ietf:params:xml:ns:netconf:base:1.0">
-        <netconf-server xmlns="urn:ietf:params:xml:ns:yang:ietf-netconf-server">
-         <listen>
-            <endpoint>
-               <name>default-ssh</name>
-               <ssh>
-                  <tcp-server-parameters>
-                     <local-address>0.0.0.0</local-address>
-                     <keepalives>
-                        <idle-time>2</idle-time>
-                        <max-probes>10</max-probes>
-                        <probe-interval>5</probe-interval>
-                     </keepalives>
-                  </tcp-server-parameters>
-                  <ssh-server-parameters>
-                     <server-identity>
-                        <host-key>
-                           <name>default-key</name>
-                           <public-key>
-                              <keystore-reference>genkey</keystore-reference>
-                           </public-key>
-                        </host-key>
-                     </server-identity>
-                     <client-authentication>
-                        <supported-authentication-methods>
-                           <publickey/>
-                           <passsword/>
-                           <other>interactive</other>
-                        </supported-authentication-methods>
-                        <users/>
-                     </client-authentication>
-                  </ssh-server-parameters>
-               </ssh>
-            </endpoint>
-         </listen>
-      </netconf-server>
-
-    </config>
-    """
-    '''
-    
+    """Edit NETCONF Config"""
+           
     #open text file in read mode
     with open(config_file, "r") as cf:
  
         #read whole file to a string
         config = cf.read()
-        
-        data = mgr.edit_config(config=config, target='running', default_operation="merge")
+        data = mgr.edit_config(config=config, target='running', default_operation="replace")
         print(data)
     
 
